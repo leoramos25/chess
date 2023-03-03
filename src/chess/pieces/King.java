0dc9d11df5e617ff151f792160850672da8ba872
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-03-03 09:59:10 -0300

    implementing possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.enums.Color;
 
@@ -16,6 +17,63 @@ public class King extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position piecePosition = new Position(0, 0);
+
+        // above
+        piecePosition.setValues(position.getRow() - 1, position.getColumn());
+        if (getBoard().positionExists(piecePosition) && canMove(piecePosition)) {
+            matrix[piecePosition.getRow()][piecePosition.getColumn()] = true;
+        }
+
+        // below
+        piecePosition.setValues(position.getRow() + 1, position.getColumn());
+        if (getBoard().positionExists(piecePosition) && canMove(piecePosition)) {
+            matrix[piecePosition.getRow()][piecePosition.getColumn()] = true;
+        }
+
+        // left
+        piecePosition.setValues(position.getRow(), position.getColumn() - 1);
+        if (getBoard().positionExists(piecePosition) && canMove(piecePosition)) {
+            matrix[piecePosition.getRow()][piecePosition.getColumn()] = true;
+        }
+
+        // right
+        piecePosition.setValues(position.getRow(), position.getColumn() + 1);
+        if (getBoard().positionExists(piecePosition) && canMove(piecePosition)) {
+            matrix[piecePosition.getRow()][piecePosition.getColumn()] = true;
+        }
+
+        // nw
+        piecePosition.setValues(position.getRow() - 1, position.getColumn() - 1);
+        if (getBoard().positionExists(piecePosition) && canMove(piecePosition)) {
+            matrix[piecePosition.getRow()][piecePosition.getColumn()] = true;
+        }
+
+        // ne
+        piecePosition.setValues(position.getRow() - 1, position.getColumn() + 1);
+        if (getBoard().positionExists(piecePosition) && canMove(piecePosition)) {
+            matrix[piecePosition.getRow()][piecePosition.getColumn()] = true;
+        }
+
+        // sw
+        piecePosition.setValues(position.getRow() + 1, position.getColumn() - 1);
+        if (getBoard().positionExists(piecePosition) && canMove(piecePosition)) {
+            matrix[piecePosition.getRow()][piecePosition.getColumn()] = true;
+        }
+
+        // se
+        piecePosition.setValues(position.getRow() + 1, position.getColumn() + 1);
+        if (getBoard().positionExists(piecePosition) && canMove(piecePosition)) {
+            matrix[piecePosition.getRow()][piecePosition.getColumn()] = true;
+        }
+
+        return matrix;
+    }
+
+    private boolean canMove(Position position) {
+        ChessPiece piece = (ChessPiece) getBoard().piece(position);
+        return piece == null || piece.getColor() != getColor();
     }
 }
